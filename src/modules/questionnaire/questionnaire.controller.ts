fdaa0aff9cf0e39081f5fc3e8c86cf33f7af01d6
arpeiks <arpeiks@gmail.com>
2024-04-11 19:03:26 +0100

    fix: get questionnaire categories

--- src/modules/questionnaire/questionnaire.controller.ts
+++ src/modules/questionnaire/questionnaire.controller.ts
@@ -3,6 +3,7 @@ import { VERSION_ONE } from '@/core/constants';
 import { QuestionnaireService } from './questionnaire.service';
 import { Body, Controller, Get, Post, Version } from '@nestjs/common';
 import { Auth as AuthGuard } from '../auth/decorators/auth.decorator';
+import { RESPONSE } from '@/core/responses';
 
 @Controller('questionnaire')
 export class QuestionnaireController {
@@ -12,13 +13,15 @@ export class QuestionnaireController {
   @Get('/admin/category')
   @AuthGuard({ roles: ['ADMIN', 'SUPER_ADMIN'] })
   async getTaskCategory() {
-    return await this.questionnaireService.HttpHandleGetTaskCategories();
+    const data = await this.questionnaireService.HttpHandleGetTaskCategories();
+    return { data, message: RESPONSE.SUCCESS };
   }
 
   @Version(VERSION_ONE)
   @Post('/questionnaire/add')
   @AuthGuard({ roles: ['ADMIN', 'SUPER_ADMIN'] })
   async addQuestionnaire(@Body() body: Dto.AddQuestionnaire[]) {
-    return await this.questionnaireService.HttpHandleAddQuestionnaire(body);
+    const data = await this.questionnaireService.HttpHandleAddQuestionnaire(body);
+    return { data, message: RESPONSE.SUCCESS };
   }
 }
