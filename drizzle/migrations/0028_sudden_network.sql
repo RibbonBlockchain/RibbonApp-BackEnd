8ccc507fb19149fb1c26aebfb84e82c6ffe0a974
arpeiks <arpeiks@gmail.com>
2024-05-12 13:42:48 +0100

    fix: migration script

--- drizzle/migrations/0028_sudden_network.sql
+++ drizzle/migrations/0028_sudden_network.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "ribbon"."questionnaire" DROP CONSTRAINT "questionnaire_category_id_questionnaire_category_id_fk";
+-- ALTER TABLE "ribbon"."questionnaire" DROP CONSTRAINT "questionnaire_category_id_questionnaire_category_id_fk";
 --> statement-breakpoint
 ALTER TABLE "ribbon"."questionnaire" ALTER COLUMN "category_id" DROP NOT NULL;
